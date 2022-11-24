c2210592d3217740523c1dc8af5b16fe8ba43460
Ajdini Sefedin <180000@CCM-Asty-Moulin.local>
2022-11-24 14:24:01 +0100

    travail termine PHP(fonctions)

--- PHP(fonctions)/index.php
+++ PHP(fonctions)/index.php
@@ -8,35 +8,36 @@
 </head>
 <body>
     <!-- Partie traitement -->
-    <?php 
-        function Factorielle($nombreDepart)
-        (
-            $resultatFactorielle = 1;
-            for ($i=1; $i < $nombreDepart ; $i++) { 
-                $resultatFactorielle = $resultatFactorielle*$i
+        <?php 
+            function Factorielle($nombreDepart)
+            {
+                $resultatFactorielle = 1;
+                    for ($i=1; $i < $nombreDepart ; $i++) { 
+                        $resultatFactorielle = $resultatFactorielle*$i;
+                    }
+                    return $resultatFactorielle;
             }
-
-            return $resultatFactorielle;
-        )
-    ?>
+                
+            
+        ?>
     <!-- Partie affichage -->
         <p>La factorielle de 7 vaut <?= Factorielle(7) ?></p>
 
     <!-- Partie traitement niveaux 2-->
-    <?php
-        function nombreLettre($chaineDepart, $caractere, $nbProbleme)
-        (
-            $longueurChaine = $chaineDepart
-            $nbProbleme = 0
-            for ($i=0; $i < $longueurChaine -1; $i++) { 
-                if ($chaineDepart[i] = $caractere) {
-                    $nbProbleme = $nbProbleme + 1
-                }
+        <?php
+            function nombreLettre($chaineDepart, $caractere)
+            {
+                $longueurChaine = strlen($chaineDepart);
+                $nbProbleme = 0;
+                    for ($i=0; $i < $longueurChaine -1; $i++) { 
+                        if ($chaineDepart[$i] = $caractere) {
+                            $nbProbleme = $nbProbleme + 1;
+                        }
+                    }
+                    return $nbProbleme;
             }
-            $text = nombreLettre;
-        )
-    ?>
+        ?>
     <!-- Partie traitement niveaux 2--> 
-            <p>voici votre reponse <?= nombreLettre(text) ?></p>
+    <p>L’utilisateur encode Bienvenue en 5TI et n, Le programme renvoie : “Il y a 3 n dans l’expression.<?= nombreLettre("Bienvenue en 5TI", "n") ?></p>
 </body>
 </html>
